047f89edfe47e946bd074e333224279c066211e1
da <dain940209@gmail.com>
2023-10-05 09:55:45 +0900

    반응형

--- script.js
+++ script.js
@@ -13,21 +13,32 @@ addButton.addEventListener("click", () => {
     const modalContent = document.createElement("div");
     modalContent.classList.add("modal-content");
 
-    const inputLabel = document.createElement("label");
-    inputLabel.textContent = "프로필 이름: ";
-    const inputField = document.createElement("input");
-    inputField.type = "text";
+    // 이름 입력 필드
+    const nameLabel = document.createElement("label");
+    nameLabel.textContent = "이름: ";
+    const nameField = document.createElement("input");
+    nameField.type = "text";
+    
+    // 나이 입력 필드
+    const ageLabel = document.createElement("label");
+    ageLabel.textContent = "나이: ";
+    const ageField = document.createElement("input");
+    ageField.type = "number";
 
     const addButtonModal = document.createElement("button");
     addButtonModal.textContent = "등록하기";
 
     // "등록하기" 버튼을 클릭할 때 프로필을 생성하고 팝업 창을 닫음
     addButtonModal.addEventListener("click", () => {
-        const profileName = inputField.value;
-        if (profileName) {
+        // const profileName = inputField.value;
+        const name = nameField.value;
+        const age = ageField.value;
+        if (name&&age) {
             const profileCard = document.createElement("div");
             profileCard.classList.add("profile-card");
-            profileCard.textContent = profileName;
+            profileCard.textContent = name;
+            profileCard.innerHTML = `<p>이름: ${name}</p><p>나이: ${age}세</p>`;
+
             profileContainer.appendChild(profileCard);
             profileCounter++;
         }
@@ -36,8 +47,10 @@ addButton.addEventListener("click", () => {
         document.body.removeChild(modal);
     });
 
-    modalContent.appendChild(inputLabel);
-    modalContent.appendChild(inputField);
+    modalContent.appendChild(nameLabel);
+    modalContent.appendChild(nameField);
+    modalContent.appendChild(ageLabel);
+    modalContent.appendChild(ageField);
     modalContent.appendChild(addButtonModal);
     modal.appendChild(modalContent);
 
